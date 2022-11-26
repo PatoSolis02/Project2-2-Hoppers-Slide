57f66adc253e2abcf455ab50b39d1a9f0183b334
pgs5983 <pgs5983@rit.edu>
2022-11-26 12:14:17 -0500

    Implemented Hoppers main program and HoppersConfig toString

--- src/puzzles/hoppers/model/HoppersConfig.java
+++ src/puzzles/hoppers/model/HoppersConfig.java
@@ -2,7 +2,63 @@ package puzzles.hoppers.model;
 
 import puzzles.common.solver.Configuration;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Scanner;
+
 // TODO: implement your HoppersConfig for the common solver
 
-public class HoppersConfig {
+public class HoppersConfig implements Configuration {
+
+    public final static char EMPTY_CELL = '.';
+
+    public final static char INVALID_CELL = '*';
+
+    private static int ROW;
+
+    private static int COL;
+
+    private String grid[][];
+
+    private HashMap<String, Array> hoppers;
+
+    public HoppersConfig(String filename) throws FileNotFoundException{
+        Scanner f = new Scanner(new File(filename));
+
+        ROW = f.nextInt();
+        COL = f.nextInt();
+
+        grid = new String[ROW][COL];
+        for(int r = 0; r < ROW; r++){
+            for(int c = 0; c < COL; c++){
+                grid[r][c] = f.next();
+            }
+        }
+    }
+
+    @Override
+    public boolean isSolution() {
+        return false;
+    }
+
+    @Override
+    public Collection<Configuration> getNeighbors() {
+        return null;
+    }
+
+    @Override
+    public String toString(){
+
+        for(int r = 0; r < ROW; r++){
+            for(int c = 0; c < COL; c++){
+                System.out.print(grid[r][c] + " ");
+            }
+            System.out.println("");
+        }
+        return "";
+    }
 }
--- src/puzzles/hoppers/solver/Hoppers.java
+++ src/puzzles/hoppers/solver/Hoppers.java
@@ -1,11 +1,32 @@
 package puzzles.hoppers.solver;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+import puzzles.hoppers.model.HoppersConfig;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Hoppers {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 1) {
             System.out.println("Usage: java Hoppers filename");
+        }
+
+        String fileName = args[0];
+        System.out.println("File: " + fileName);
+
+        HoppersConfig initConfig = new HoppersConfig(fileName);
+        initConfig.toString();
+
+        ArrayList<Configuration> solution = Solver.solver(initConfig);
+
+        if(solution.isEmpty()){
+            System.out.println("No solution found!");
+        } else {
+            for(int i = 0; i < solution.size(); i++){
+                System.out.println("Step " + i + ": \n" + solution.get(i).toString());
+            }
         }
     }
 }
