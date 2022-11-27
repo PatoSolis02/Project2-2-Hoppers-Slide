6a5cf4b7b075661effb5325115e7582b92b6a654
pgs5983 <pgs5983@rit.edu>
2022-11-27 01:00:49 -0500

    Added a Hopper class object

--- src/puzzles/hoppers/model/HoppersConfig.java
+++ src/puzzles/hoppers/model/HoppersConfig.java
@@ -5,10 +5,7 @@ import puzzles.common.solver.Configuration;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 // TODO: implement your HoppersConfig for the common solver
 
@@ -24,7 +21,6 @@ public class HoppersConfig implements Configuration {
 
     private String grid[][];
 
-    private HashMap<String, Array> hoppers;
 
     public HoppersConfig(String filename) throws FileNotFoundException{
         Scanner f = new Scanner(new File(filename));
@@ -35,9 +31,14 @@ public class HoppersConfig implements Configuration {
         grid = new String[ROW][COL];
         for(int r = 0; r < ROW; r++){
             for(int c = 0; c < COL; c++){
-                grid[r][c] = f.next();
+                String next = f.next();
+                grid[r][c] = next;
             }
         }
+    }
+
+    public HoppersConfig(){
+
     }
 
     @Override
@@ -47,7 +48,29 @@ public class HoppersConfig implements Configuration {
 
     @Override
     public Collection<Configuration> getNeighbors() {
+        List<Configuration> successors = new LinkedList<>();
+        for(int r = 0; r < ROW; r++){
+            for(int c = 0; c < COL; c++){
+                if(grid[r][c].equals("G") || grid[r][c].equals("R")){
+                    successors.add(checkMove(r, c));
+                }
+            }
+        }
+
+        return successors;
+    }
+
+    public Configuration checkMove(int row, int col){
+        if(row % 2 == 0){
+            // check the 8 possible jumps spots
+        }
+        // check the 4 possible jumps spots
+
         return null;
+    }
+
+    public boolean isValid(){
+        return false;
     }
 
     @Override
