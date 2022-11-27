64eba90ba914d3b1feea5d8e72836c626f21bb9a
Vywer <54511719+Vywer@users.noreply.github.com>
2022-11-27 14:10:00 -0500

    Worked on Slide Initial Configuration - Note: Something weird is happening in configuration creation and unsure what. Is preventing proper gird recognition. Will check later

--- src/puzzles/slide/model/SlideConfig.java
+++ src/puzzles/slide/model/SlideConfig.java
@@ -1,4 +1,69 @@
 package puzzles.slide.model;
 
-public class SlideConfig  {
+import puzzles.common.solver.Configuration;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class SlideConfig implements Configuration {
+    private int row;
+    private int column;
+    private final String[][] grid;
+
+    public SlideConfig(String filename) throws FileNotFoundException {
+        Scanner f = new Scanner(new File(filename));
+        this.row = f.nextInt();
+        this.column = f.nextInt();
+        this.grid = new String[this.row][this.column];
+        for (int row = 0; row<this.row; row++) {
+            for (int col = 0; col<this.column; col++){
+                this.grid[row][col] = f.next();
+            }
+        }
+        f.close();
+    }
+
+    public SlideConfig(SlideConfig copy) {
+        this.row = copy.row;
+        this.column = copy.column;
+        this.grid = new String[row][column];
+        for (int row=0; row<this.row; row++) {
+            System.arraycopy(copy.grid[row], 0, this.grid[row], 0, row);
+        }
+    }
+
+    @Override
+    public boolean isSolution() {
+        return false;
+    }
+
+    @Override
+    public Collection<Configuration> getNeighbors() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        for (int row = 0; row< this.row; this.row++) {
+            for (int col = 0; col< column; column++) {
+                System.out.println(grid[row][col]);
+                output.append(grid[row][col]).append(" ");
+            }
+        }
+        return output.toString();
+    }
 }
--- src/puzzles/slide/solver/Slide.java
+++ src/puzzles/slide/solver/Slide.java
@@ -1,9 +1,29 @@
 package puzzles.slide.solver;
 
+import puzzles.common.solver.Configuration;
+import puzzles.common.solver.Solver;
+import puzzles.slide.model.SlideConfig;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class Slide {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 1) {
             System.out.println("Usage: java Slide filename");
+        } else {
+            String filename = args[0];
+            SlideConfig initConfig = new SlideConfig(filename);
+            System.out.println(initConfig);
+//            List<Configuration> path = Solver.solver(initConfig);
+//            System.out.println("File: " + filename);
+//            if (path.isEmpty()) {
+//                System.out.println("No solution found!");
+//            } else {
+//                for (int i = 0; i < path.size(); i++) {
+//                    System.out.println("Step " + i + ": " + path.get(i));
+//                }
+//            }
         }
     }
 }
