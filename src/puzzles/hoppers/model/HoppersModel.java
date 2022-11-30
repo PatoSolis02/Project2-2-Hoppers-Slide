22f6c759835e6ecd6430f82d5fef033fcbe550e3
pgs5983 <pgs5983@rit.edu>
2022-11-30 13:22:56 -0500

    Working on model and PTUI

--- src/puzzles/hoppers/model/HoppersModel.java
+++ src/puzzles/hoppers/model/HoppersModel.java
@@ -1,10 +1,13 @@
 package puzzles.hoppers.model;
 
 import puzzles.common.Observer;
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -19,8 +22,16 @@ public class HoppersModel {
         OCCUPIED
     }
 
+    public enum Status{
+        NOT_OVER,
+        WON,
+        LOST
+    }
+
     /** the current configuration */
     private HoppersConfig currentConfig;
+    private Status status;
+
 
     /**
      * The view calls this to add itself as an observer.
@@ -44,4 +55,58 @@ public class HoppersModel {
     public HoppersModel(String filename) throws IOException {
         this.currentConfig = new HoppersConfig(filename);
     }
+
+    public Status getStatus() {return status;}
+
+    public void hint(){
+
+        ArrayList<Configuration> solution = Solver.solver(this.currentConfig);
+        if(solution.isEmpty()){
+            alertObservers("There is no solution!");
+        } else {
+            this.currentConfig = (HoppersConfig) solution.get(1);
+            alertObservers("Successful hint!");
+        }
+    }
+
+    public void load(String filename){
+        try{
+            this.currentConfig = new HoppersConfig(filename);
+            alertObservers("New file loaded!");
+        } catch (FileNotFoundException noFile){
+            alertObservers("File could not be read.");
+        }
+    }
+
+    public void select(int row, int col){
+
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+
+        String[][] currGrid = currentConfig.getGrid();
+
+        builder.append(' ');
+        for(int c = 0; c < currentConfig.getCOL(); c++){
+            builder.append(" " + c + " ");
+        }
+        builder.append('\n');
+
+        builder.append("--".repeat(Math.max(0, currentConfig.getCOL())));
+        builder.append('\n');
+
+        for(int r = 0; r < currentConfig.getROW(); r++){
+            builder.append(r + "|");
+            for(int c = 0; c < currentConfig.getCOL(); c++){
+                builder.append(" " + currGrid[r][c] + " ");
+            }
+            builder.append("\n");
+        }
+
+
+        return builder.toString();
+    }
+
 }
--- src/puzzles/hoppers/ptui/HoppersPTUI.java
+++ src/puzzles/hoppers/ptui/HoppersPTUI.java
@@ -3,26 +3,29 @@ package puzzles.hoppers.ptui;
 import puzzles.common.Observer;
 import puzzles.hoppers.model.HoppersModel;
 
+import java.io.IOException;
+
 public class HoppersPTUI implements Observer<HoppersModel, String> {
     private HoppersModel model;
 
 
-    public HoppersPTUI(String filename){
+    public HoppersPTUI(String filename) throws IOException {
         this.model = new HoppersModel(filename);
         initializeView();
     }
 
     @Override
     public void update(HoppersModel model, String msg) {
+        System.out.println(msg);
+        System.out.println(this.model);
     }
 
     private void initializeView(){this.model.addObserver(this);}
 
     public void run(){
-
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             System.out.println("Usage: java HoppersPTUI filename");
         } else {
